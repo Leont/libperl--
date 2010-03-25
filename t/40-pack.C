3f790ba46c767bfbf2c1305dfa133e73bb65e66b
Leon Timmermans <fawaka@gmail.com>
2010-03-25 20:09:15 +0200

    Added a few unit tests for pack

--- t/40-pack.C
+++ t/40-pack.C
@@ -6,5 +6,16 @@ using namespace perl;
 using namespace TAP;
 
 int main() {
-	plan(skip_all, "Tests not implemented yet");
+	plan(3);
+
+	Interpreter universe;
+	String packed = universe.pack("Nni", 1001, 32768, -4096);
+	Array unpacked = packed.unpack("Nni");
+
+	is(unpacked[0], 1001, "unpacked[0] == 1001");
+	is(unpacked[1], 32768, "unpacked[0] == 32768");
+	is(unpacked[2], -4096, "unpacked[0] == -4096");
+	// I need a decent substr for more complicated tests.
+
+	return exit_status();
 }
