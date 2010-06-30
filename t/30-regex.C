e375687bfba6fbea6196a61f1af764759813daac
Leon Timmermans <fawaka@gmail.com>
2010-06-30 15:48:48 +0200

    Added tests and fixed bugs for regexps

--- t/30-regex.C
+++ t/30-regex.C
@@ -7,10 +7,29 @@ using namespace perl;
 using namespace TAP;
 
 int main() {
-	TEST_START(2);
+	TEST_START(9);
 	Interpreter universe;
 	Regex first = universe.regex("a*b");
 	ok(first.match(universe.value_of("aab")), "\"aab\" =~ /a*b/;");
 	not_ok(first.match("aaa"), "\"aaa\" !~ /a*b/;");
+
+	Array matches = universe.regex("a+").match("abaabaaabaaba", "g");
+
+	is(matches, 5u, "Match returned elements");
+
+	is(matches[1], "aa");
+
+	String victim = universe.value_of("aab");
+
+	ok(victim.match(first), "Victim matches");
+
+	ok(victim.substitute(first, "saab"), "Victim substitutes");
+
+	is(victim, "saab", "Victim now is saab");
+
+	ok(victim.substitute(universe.regex("(a*b)"), "foo$1bar"), "Victim substitutes complexly");
+
+	is(victim, "sfooaabbar", "Complex substitution works");
+
 	TEST_END;
 }
