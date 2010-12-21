9b0d146c3a7d9a3b0bea86ca3c5cbec70ed1c27c
Leon Timmermans <fawaka@gmail.com>
2010-12-21 19:34:32 +0100

    Converted inject_methods to inject_roles in Base

--- inc/Library/Build/Base.pm
+++ inc/Library/Build/Base.pm
@@ -72,12 +72,6 @@ sub _linker_flags {
 
 # XXX
 
-sub cbuilder {
-	my $self = shift;
-	require ExtUtils::CBuilder;
-	return $self->{builder} ||= ExtUtils::CBuilder->new(quiet => $self->stash('verbose') < 0)
-}
-
 my $my_system = $^O eq 'MSWin32'
 ? sub {
 	my ($self, $exec, $input, $output) = @_;
@@ -101,201 +95,193 @@ my $my_system = $^O eq 'MSWin32'
 	return;
 };
 
-sub create_by_system {
-	my ($self, $exec, $input, $output) = @_;
-	if (not -e $output or -M $input < -M $output) {
-		$my_system->($self, $exec, $input, $output);
-	}
-	return;
-}
-
-sub process_cpp {
-	my ($self, $input, $output) = @_;
-	$self->create_by_system([ $Config{cpp}, split(/ /, $Config{ccflags}), '-I' . catdir($Config{archlibexp}, 'CORE') ], $input, $output);
-	return;
-}
-
-sub process_perl {
-	my ($self, $input, $output) = @_;
-	$self->create_by_system([ $^X, '-T' ], $input, $output);
-	return;
-}
-
-sub create_dir {
-	my ($self, @dirs) = @_;
-	mkpath(\@dirs, $self->stash('verbose') >= 0, $SECURE);
-	return;
-}
-
-sub copy_files {
-	my ($self, $source, $destination) = @_;
-	if (-d $source) {
-		$self->create_dir($destination);
-		opendir my $dh, $source or croak "Can't open dir $source: $!";
-		for my $filename (readdir $dh) {
-			next if $filename =~ / \A \. /xms;
-			$self->copy_files(catfile($source, $filename), catfile($destination, $filename));
+my %base_methods = (
+	cbuilder => sub {
+		my $self = shift;
+		require ExtUtils::CBuilder;
+		return $self->{builder} ||= ExtUtils::CBuilder->new(quiet => $self->stash('verbose') < 0)
+	},
+	create_by_system => sub {
+		my ($self, $exec, $input, $output) = @_;
+		if (not -e $output or -M $input < -M $output) {
+			$my_system->($self, $exec, $input, $output);
 		}
-		closedir $dh;
-	}
-	elsif (-f $source) {
-		$self->create_dir(dirname($destination));
-		if (not -e $destination or -M $source < -M $destination) {
-			print "cp $source $destination\n" if $self->stash('verbose') >= 0;
-			copy($source, $destination) or croak "Could not copy '$source' to '$destination': $!";
+		return;
+	},
+	process_cpp => sub {
+		my ($self, $input, $output) = @_;
+		$self->create_by_system([ $Config{cpp}, split(/ /, $Config{ccflags}), '-I' . catdir($Config{archlibexp}, 'CORE') ], $input, $output);
+		return;
+	},
+	process_perl => sub {
+		my ($self, $input, $output) = @_;
+		$self->create_by_system([ $^X, '-T' ], $input, $output);
+		return;
+	},
+	create_dir => sub {
+		my ($self, @dirs) = @_;
+		mkpath(\@dirs, $self->stash('verbose') >= 0, $SECURE);
+		return;
+	},
+	copy_files => sub {
+		my ($self, $source, $destination) = @_;
+		if (-d $source) {
+			$self->create_dir($destination);
+			opendir my $dh, $source or croak "Can't open dir $source: $!";
+			for my $filename (readdir $dh) {
+				next if $filename =~ / \A \. /xms;
+				$self->copy_files(catfile($source, $filename), catfile($destination, $filename));
+			}
+			closedir $dh;
 		}
-	}
-	return;
-}
+		elsif (-f $source) {
+			$self->create_dir(dirname($destination));
+			if (not -e $destination or -M $source < -M $destination) {
+				print "cp $source $destination\n" if $self->stash('verbose') >= 0;
+				copy($source, $destination) or croak "Could not copy '$source' to '$destination': $!";
+			}
+		}
+		return;
+	},
+	build_objects => sub {
+		my ($self, %args) = @_;
 
-sub build_objects {
-	my ($self, %args) = @_;
+		my $input_dir  = $args{input_dir} || '.';
+		my $tempdir    = $args{temp_dir}  || '_build';
+		my @raw_files  = _get_input_files(@args{qw/input_files input_dir/});
+		my %object_for = map { (catfile($input_dir, $_) => catfile($tempdir, $self->cbuilder->object_file($_))) } @raw_files;
+		my @input_dirs = ( (defined $self->stash('include_dir') ? @{ $self->stash('include_dir') } : ()), (defined $args{include_dirs} ? @{ $args{include_dirs} } : ()));
 
-	my $input_dir  = $args{input_dir} || '.';
-	my $tempdir    = $args{temp_dir}  || '_build';
-	my @raw_files  = _get_input_files(@args{qw/input_files input_dir/});
-	my %object_for = map { (catfile($input_dir, $_) => catfile($tempdir, $self->cbuilder->object_file($_))) } @raw_files;
-	my @input_dirs = ( (defined $self->stash('include_dir') ? @{ $self->stash('include_dir') } : ()), (defined $args{include_dirs} ? @{ $args{include_dirs} } : ()));
+		for my $source_file (sort keys %object_for) {
+			my $object_file = $object_for{$source_file};
+			next if -e $object_file and -M $source_file > -M $object_file;
+			$self->create_dir(dirname($object_file));
+			$self->cbuilder->compile(
+				source               => $source_file,
+				object_file          => $object_file,
+				'C++'                => $args{'C++'},
+				include_dirs         => \@input_dirs,
+				extra_compiler_flags => $args{cc_flags} || _compiler_flags(),
+			);
+		}
+		return values %object_for;
+	},
+	build_library => sub {
+		my ($self, %args) = @_;
 
-	for my $source_file (sort keys %object_for) {
-		my $object_file = $object_for{$source_file};
-		next if -e $object_file and -M $source_file > -M $object_file;
-		$self->create_dir(dirname($object_file));
-		$self->cbuilder->compile(
-			source               => $source_file,
-			object_file          => $object_file,
-			'C++'                => $args{'C++'},
-			include_dirs         => \@input_dirs,
-			extra_compiler_flags => $args{cc_flags} || _compiler_flags(),
-		);
-	}
-	return values %object_for;
-}
+		my @objects      = $self->build_objects(%args);
 
-sub build_library {
-	my ($self, %args) = @_;
+		my $output_dir   = $args{output_dir} || 'blib';
+		my $library_file = $args{libfile} || catfile($output_dir, 'so', 'lib' . $self->cbuilder->lib_file($args{name}));
+		my $linker_flags = _linker_flags($args{libs}, $args{libdirs}, append => $args{linker_append}, 'C++' => $args{'C++'});
+		$self->create_dir(dirname($library_file));
+		$self->cbuilder->link(
+			lib_file           => $library_file,
+			objects            => \@objects,
+			extra_linker_flags => $linker_flags,
+			module_name        => $args{name},
+		) if not -e $library_file or grep { (-M $_ < -M $library_file ) } @objects;
+		return;
+	},
+	build_executable => sub {
+		my ($self, %args) = @_;
 
-	my @objects      = $self->build_objects(%args);
+		my @objects      = $self->build_objects(%args);
+		my $linker_flags = _linker_flags($args{libs}, $args{libdirs}, append => $args{linker_append}, 'C++' => $args{'C++'});
+		$self->create_dir(dirname($args{output}));
+		$self->cbuilder->link_executable(
+			objects            => \@objects,
+			exe_file           => $args{output},
+			extra_linker_flags => $linker_flags,
+			'C++'              => $args{'C++'},
+		) if not -e $args{output} or grep { (-M $_ < -M $args{output}) } @objects;
+		return;
+	},
+	pod2man => sub {
+		my ($self, $source, $dest) = @_;
+		return if -e $dest and -M $source > -M $dest;
+		$self->create_dir(dirname($dest));
+		print "pod2man $source $dest\n" if $self->stash('verbose') >= 0;
+		$self->{pod_parser} ||= Pod::Man->new;
+		$self->{pod_parser}->parse_from_file($source, $dest);
+		return;
+	},
+	run_tests => sub {
+		my ($self, @test_goals) = @_;
+		my $library_var = $self->stash('library_var') || $Config{ldlibpthname};
+		printf "Report %s\n", strftime('%y%m%d-%H:%M', localtime) if $self->stash('verbose') > -2;
+		my $harness = TAP::Harness->new({
+			verbosity => $self->stash('verbose'),
+			exec => sub {
+				my (undef, $file) = @_;
+				return -B $file ? [ $file ] : [ $^X, '-T', '-I' . catdir(qw/blib lib/), $file ];
+			},
+			merge => 1,
+			color => -t STDOUT,
+		});
 
-	my $output_dir   = $args{output_dir} || 'blib';
-	my $library_file = $args{libfile} || catfile($output_dir, 'so', 'lib' . $self->cbuilder->lib_file($args{name}));
-	my $linker_flags = _linker_flags($args{libs}, $args{libdirs}, append => $args{linker_append}, 'C++' => $args{'C++'});
-	$self->create_dir(dirname($library_file));
-	$self->cbuilder->link(
-		lib_file           => $library_file,
-		objects            => \@objects,
-		extra_linker_flags => $linker_flags,
-		module_name        => $args{name},
-	) if not -e $library_file or grep { (-M $_ < -M $library_file ) } @objects;
-	return;
-}
+		my $ld_dir = catdir(qw/blib so/);
+		local $ENV{$library_var} = $ld_dir if -d $ld_dir;
+		return $harness->runtests(@test_goals);
+	},
+	remove_tree => sub {
+		my ($self, @files) = @_;
+		rmtree(\@files, $self->stash('verbose') >= 0, 0);
+		return;
+	},
+	find_files => sub {
+		my ($self, $dir, $regexp) = @_;
+		my @ret;
+		find({
+			wanted   => sub {
+				push @ret, $_ if $_ =~ $regexp; 
+			},
+			no_chdir => 1,
+		} , $dir) if -d $dir;
+		@ret = sort @ret;
+		return @ret;
+	},
+	prompt => sub {
+		my ($self, $question, $default) = @_;
+		return $default if $ENV{PERL_MM_USE_DEFAULT} or not -t STDIN and eof STDIN;
+		print "$question [$default] ";
+		my $answer = <STDIN>;
+		return $default if not defined $answer;
+		chomp $answer;
+		return $answer;
+	},
+	yes_no => sub {
+		my ($self, $question, $default) = @_;
+		while (1) {
+			my $answer = $self->prompt($question, $default ? 'y' : 'n');
+			return 1 if $answer =~ / \A y ( es )? \z /xmsi;
+			return 0 if $answer =~ / \A n ( o  )? \z /xmsi;
+			print "Please answer 'y' or 'n'.\n";
+		}
+	},
+	remove_dirty_files => sub {
+		my ($self, @categories) = @_;
+		my @keys = map { $_ eq 'all' ? keys %{ $self->{dirty_files} } : $_ } @categories;
+		my @files = map { @{ $self->{dirty_files}{$_} } } sort @keys;
+		$self->remove_tree(@files);
+		return;
+	},
+	register_dirty => sub {
+		my ($self, %files_map) = @_;
+		while (my ($category, $files) = each %files_map) {
+			push @{ $self->{dirty_files}{$category} }, @{$files};
+		}
+		return;
+	},
+	register_paths => sub {
+		my ($self, %paths) = @_;
+		while (my ($source, $destination) = each %paths) {
+			$self->{install_paths}{"blib/$source"} = $destination;
+		}
+		return;
+	},
+);
 
-sub build_executable {
-	my ($self, %args) = @_;
-
-	my @objects      = $self->build_objects(%args);
-	my $linker_flags = _linker_flags($args{libs}, $args{libdirs}, append => $args{linker_append}, 'C++' => $args{'C++'});
-	$self->create_dir(dirname($args{output}));
-	$self->cbuilder->link_executable(
-		objects            => \@objects,
-		exe_file           => $args{output},
-		extra_linker_flags => $linker_flags,
-		'C++'              => $args{'C++'},
-	) if not -e $args{output} or grep { (-M $_ < -M $args{output}) } @objects;
-	return;
-}
-
-sub pod2man {
-	my ($self, $source, $dest) = @_;
-	return if -e $dest and -M $source > -M $dest;
-	$self->create_dir(dirname($dest));
-	print "pod2man $source $dest\n" if $self->stash('verbose') >= 0;
-	$self->{pod_parser} ||= Pod::Man->new;
-	$self->{pod_parser}->parse_from_file($source, $dest);
-	return;
-}
-
-sub run_tests {
-	my ($self, @test_goals) = @_;
-	my $library_var = $self->stash('library_var') || $Config{ldlibpthname};
-	printf "Report %s\n", strftime('%y%m%d-%H:%M', localtime) if $self->stash('verbose') > -2;
-	my $harness = TAP::Harness->new({
-		verbosity => $self->stash('verbose'),
-		exec => sub {
-			my (undef, $file) = @_;
-			return -B $file ? [ $file ] : [ $^X, '-T', '-I' . catdir(qw/blib lib/), $file ];
-		},
-		merge => 1,
-		color => -t STDOUT,
-	});
-
-	my $ld_dir = catdir(qw/blib so/);
-	local $ENV{$library_var} = $ld_dir if -d $ld_dir;
-	return $harness->runtests(@test_goals);
-}
-
-sub remove_tree {
-	my ($self, @files) = @_;
-	rmtree(\@files, $self->stash('verbose') >= 0, 0);
-	return;
-}
-
-sub find_files {
-	my ($self, $dir, $regexp) = @_;
-	my @ret;
-	find({
-		wanted   => sub {
-			push @ret, $_ if $_ =~ $regexp; 
-		},
-		no_chdir => 1,
-	} , $dir) if -d $dir;
-	@ret = sort @ret;
-	return @ret;
-}
-
-sub prompt {
-	my ($self, $question, $default) = @_;
-	return $default if $ENV{PERL_MM_USE_DEFAULT} or not -t STDIN and eof STDIN;
-	print "$question [$default] ";
-	my $answer = <STDIN>;
-	return $default if not defined $answer;
-	chomp $answer;
-	return $answer;
-}
-
-sub yes_no {
-	my ($self, $question, $default) = @_;
-	while (1) {
-		my $answer = $self->prompt($question, $default ? 'y' : 'n');
-		return 1 if $answer =~ / \A y ( es )? \z /xmsi;
-		return 0 if $answer =~ / \A n ( o  )? \z /xmsi;
-		print "Please answer 'y' or 'n'.\n";
-	}
-}
-
-sub remove_dirty_files {
-	my ($self, @categories) = @_;
-	my @keys = map { $_ eq 'all' ? keys %{ $self->{dirty_files} } : $_ } @categories;
-	my @files = map { @{ $self->{dirty_files}{$_} } } sort @keys;
-	$self->remove_tree(@files);
-	return;
-}
-
-sub register_dirty {
-	my ($self, %files_map) = @_;
-	while (my ($category, $files) = each %files_map) {
-		push @{ $self->{dirty_files}{$category} }, @{$files};
-	}
-	return;
-}
-
-sub register_paths {
-	my ($self, %paths) = @_;
-	while (my ($source, $destination) = each %paths) {
-		$self->{install_paths}{"blib/$source"} = $destination;
-	}
-	return;
-}
 
 my %default_actions = (
 	lib       => sub {
@@ -422,13 +408,7 @@ my %install_base_relpaths = (
 
 sub mixin {
 	my $builder = shift;
-	while (my ($name, $glob) = each %Library::Build::Base::) {
-		next if $name =~ / \A _ /x;
-		next if $name eq 'mixin';
-		next if not defined *$glob{CODE};
-		next if *$glob{PACKAGE} ne 'Library::Build::Base';
-		$builder->inject_method($name, *$glob{CODE});
-	}
+	$builder->inject_roles({ base => \%base_methods });
 	$builder->stash(verbose => 0);
 	$builder->register_actions(%default_actions);
 	$builder->register_dirty(
