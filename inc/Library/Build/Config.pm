e7d72090cf006b0c62e99aa5d04ade1e14182582
Leon Timmermans <fawaka@gmail.com>
2010-12-23 23:57:35 +0100

    Refactoring of configfile parsing

--- inc/Library/Build/Config.pm
+++ inc/Library/Build/Config.pm
@@ -6,51 +6,48 @@ use warnings FATAL => 'all';
 
 our $VERSION = '0.003';
 
-use Carp qw/croak/;
+use Carp qw/croak carp/;
 use File::Spec::Functions qw/catfile/;
 
 my $NOTFOUND = -1;
 
-sub parse_line {
-	my ($action, $line) = @_;
+my @files = (
+	($ENV{MODULEBUILDRC} ? $ENV{MODULEBUILDRC}                         : ()),
+	($ENV{HOME} ?          catfile($ENV{HOME}, '.modulebuildrc')       : ()),
+	($ENV{USERPROFILE} ?   catfile($ENV{USERPROFILE}, '.modulebuldrc') : ()),
+);
 
-	for ($line) {
-		next if / ^ \# /xms;
-		s{ \n \s+ }{ }x;
-		next if / ^ \s* $ /xms;
-
-
-		if (my ($name, $args) = m/ \A \s* (\* | \w+ ) \s+ (.*?) \s* \z /xms) {
-			my @args = split / \s+ /x, $args;
-			return @args if $name eq $action or $name eq '*';
-		}
-		else {
-			croak "Can't parse line '$_'";
-		}
-	}
-	return;
+sub slurp {
+	my $filename = shift;
+	open my $fh, '<', $filename or croak "Couldn't open configuration file '$filename': $!";
+	my $content = do { local $/ = undef, <$fh> };
+	close $fh or croak "Can't close $filename: $!";
+	return $content;
 }
 
 sub read_config {
 	my $action = shift;
-
 	my @ret;
 
-	my @files = (
-		($ENV{MODULEBUILDRC} ? $ENV{MODULEBUILDRC}                        : ()),
-		($ENV{HOME} ?          catfile($ENV{HOME},'.modulebuildrc')       : ()),
-		($ENV{USERPROFILE} ?   catfile($ENV{USERPROFILE},'.modulebuldrc') : ()),
-	);
 	FILE:
-	for my $file (@files) {
-		next FILE if not -e $file;
+	for my $filename (@files) {
+		next FILE if not -e $filename;
 
-		open my $fh, '<', $file or croak "Couldn't open configuration file '$file': $!";
-		my @lines = split / \n (?! \s) /xms, do { local $/ = undef, <$fh> };
-		close $fh or croak "Can't close $file: $!";
-		for my $line (@lines) {
-			push @ret, parse_line($action, $line);
+		my $content = slurp($filename);
+
+		$content =~ s/ (?<!\\) \# [^\n]*//gxm; # Remove comments
+		$content =~ s/ \n [ \t\f]+ / /gx;      # Join multi-lines
+		LINE:
+		for my $line (split /\n/, $content) {
+			next LINE if $line =~ / \A \s* \z /xms;  # Skip empty lines
+			if (my ($name, $args) = $line =~ m/ \A \s* (\* | [\w.-]+ ) \s+ (.*?) \s* \z /xms) {
+				push @ret, split / \s+ /x, $args if $name eq $action or $name eq '*';
+			}
+			else {
+				carp "Can't parse line '$line'";
+			}
 		}
+		last FILE;
 	}
 	return @ret;
 }
