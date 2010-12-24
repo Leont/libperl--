a79625b5c10ec5b40ad3946da343cbf64a534005
Leon Timmermans <fawaka@gmail.com>
2010-12-24 12:42:14 +0100

    Added distdir action, fixed distcheck

--- inc/Library/Build/Author.pm
+++ inc/Library/Build/Author.pm
@@ -9,7 +9,8 @@ our $VERSION = '0.003';
 use Archive::Tar;
 use Carp 'croak';
 use Config;
-use ExtUtils::Manifest qw/maniread manicheck mkmanifest/;
+use ExtUtils::Manifest qw/maniread fullcheck mkmanifest manicopy/;
+$ExtUtils::Manifest::Quiet = 1;
 
 my $NONREADABLE = ~oct 22;
 
@@ -26,12 +27,21 @@ my %author_actions = (
 		print "tar xjf $release_name.tar.gz @files\n" if $builder->stash('verbose') >= 0;
 		$arch->write("$release_name.tar.gz", COMPRESS_GZIP, $release_name);
 	},
+	distdir   => sub {
+		my $builder = shift;
+		$builder->dispatch('build');
+		my $manifest = maniread() or croak 'No MANIFEST found';
+		my $release_name = $builder->name . '-' . $builder->version;
+		$builder->create_dir($release_name);
+		manicopy($manifest, $release_name, 'cp');
+	},
 	manifest  => sub {
 		mkmanifest();
 	},
 	distcheck => sub {
-		my @missing = manicheck();
-		croak "Missing files @missing" if @missing;
+		my ($missing, $extra) = fullcheck();
+		croak "Missing on filesystem: @{$missing}" if @{$missing};
+		croak "Missing in MANIFEST: @{$extra}" if @{$extra}
 	},
 );
 
